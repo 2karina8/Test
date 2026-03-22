50d8d9862be0ec21a26d95b53a1c8dde3f4ac5b4
Karina <lomachinskayaaa@gmail.com>
2026-03-22 21:33:53 +0200

    added visitAdd and ReachedVisit

--- Gym/Program.cs
+++ Gym/Program.cs
@@ -9,24 +9,45 @@ namespace Gym
 {
     class GymMember
     {
-        private readonly int id;
-        private string name;
-        private int visitcounter;
+        private readonly int id; // айдішка
+        private string name; // ім'я
+        private int visitcounter; // к-сть відвідувань
 
-        private static int membercounter = 0;
-        public GymMember(int id, string name, int visitcounter)
+        private static int membercounter = 0; // рахувати мемберів
+
+        // конструктори 
+        public GymMember(int id, string name, int visitcounter) // конструктор для visitcounter =! 0
         {
             this.id = id;
             this.name = name;
             this.visitcounter = visitcounter;
             membercounter++;
         }
-        public GymMember(int id, string name)
+        public GymMember(int id, string name)   // конструктор для visitcounter = 0
         {
             this.id = id;
             this.name = name;
             this.visitcounter = 0;
             membercounter++;
         }
+        // метод додавання відвідування та метод перевірки, чи досягнуто певної кількості відвідувань.
+        public void Visit() // для підрахунку к-сті відвідувачів
+        {
+            visitcounter++;
+        }
+        public bool Reachedvisit(int cnst) // cnst - consta - потрібна кількість відвідувачів
+        // логіка: досягнення певної кількості відвідувань
+        {
+            if (visitcounter >= cnst)
+            {
+                return true;
+            }
+            else 
+                return false;
+        }
+
+
+
+
     }
 }
